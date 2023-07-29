5b9b943e85ca98668eb8a5dded7626198a93ef4d
kgcitizen <bekturboobekov@gmail.com>
2023-07-30 02:05:47 +0600

    Remove the logic that plays {number} of rounds

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ function getComputerChoice() {
 
   switch (randomChoice) {
     case 1:
-      return "rock";
+      return 'rock';
     case 2:
-      return "paper";
+      return 'paper';
     case 3:
-      return "scissors";
+      return 'scissors';
     default:
-      console.warn("Something went wrong!");
+      console.warn('Something went wrong!');
   }
 }
 
@@ -18,7 +18,7 @@ function getComputerChoice() {
 //? Input --> 2. (optional) Computer selection, string
 //? Return --> Alert winner message (or draft), string
 function startRound(
-  playerSelection = prompt("Rock, paper, or scissors? Type your move: "),
+  playerSelection = prompt('Rock, paper, or scissors? Type your move: '),
   computerSelection = getComputerChoice()
 ) {
   let ps = playerSelection.toLowerCase();
@@ -26,58 +26,39 @@ function startRound(
 
   switch (true) {
     case ps === cs:
-      return "Draft!";
-    case ps === "rock" && cs === "scissors":
-      return "You win! Rock beats Scissors";
-    case ps === "paper" && cs === "rock":
-      return "You win! Paper beats Rock";
-    case ps === "scissors" && cs === "paper":
-      return "You win! Scissors beats Paper";
-    case cs === "rock" && ps === "scissors":
-      return "You lose! Rock beats Scissors";
-    case cs === "paper" && ps === "rock":
-      return "You lose! Paper beats Rock";
-    case cs === "scissors" && ps === "paper":
-      return "You lose! Scissors beats Paper";
+      return 'Draft!';
+    case ps === 'rock' && cs === 'scissors':
+      return 'You win! Rock beats Scissors';
+    case ps === 'paper' && cs === 'rock':
+      return 'You win! Paper beats Rock';
+    case ps === 'scissors' && cs === 'paper':
+      return 'You win! Scissors beats Paper';
+    case cs === 'rock' && ps === 'scissors':
+      return 'You lose! Rock beats Scissors';
+    case cs === 'paper' && ps === 'rock':
+      return 'You lose! Paper beats Rock';
+    case cs === 'scissors' && ps === 'paper':
+      return 'You lose! Scissors beats Paper';
     default:
-      return "Something went wrong";
+      return 'Something went wrong';
   }
 }
 
 //? Input --> (optional) number of rounds, integer
 //? Output --> 1. Alert round winner message, string
 //? Output --> 2. Alert game winner message, string
-function game(rounds = 5) {
-  let playerScore = 0;
-  let computerScore = 0;
-
+function game() {
   let getRoundWinner = (str) => {
-    if (str.includes("win")) {
-      playerScore++;
-      return "Player";
-    } else if (str.includes("lose")) {
-      computerScore++;
-      return "Computer";
-    } else return "No one";
+    if (str.includes('win')) {
+      return 'Player';
+    } else if (str.includes('lose')) {
+      return 'Computer';
+    } else return 'No one';
   };
 
-  for (i = 1; i <= 5; i++) {
-    let roundResultMessage = startRound();
-    let winner = getRoundWinner(roundResultMessage);
+  let roundResultMessage = startRound();
 
-    alert(
-      `Round ${i} of ${rounds}\n${roundResultMessage}\nRound winner: ${winner}.`
-    );
-  }
-
-  if (playerScore > computerScore) {
-    alert("Player won the game!");
-  } else if (computerScore > playerScore) {
-    alert("Computer won the game!");
-  } else {
-    alert("No winner in this game!")
-  }
+  alert(`${roundResultMessage}`);
 }
-
 
 game();
